c3c4350c8290e0e53a4a3d7b7cc5d606dee0e770
gleblom <139051065+gleblom@users.noreply.github.com>
2023-08-20 15:49:59 +0300

    Add files via upload

--- src/main/java/net/gleb/testmod/renderer/entity/StandEntityRenderer.java
+++ src/main/java/net/gleb/testmod/renderer/entity/StandEntityRenderer.java
@@ -4,6 +4,7 @@ import net.gleb.testmod.TestMod;
 import net.gleb.testmod.world.entity.stand.StandEntity;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
--- src/main/java/net/gleb/testmod/world/entity/stand/StandEntity.java
+++ src/main/java/net/gleb/testmod/world/entity/stand/StandEntity.java
@@ -1,16 +1,9 @@
 package net.gleb.testmod.world.entity.stand;
 
-import net.gleb.testmod.item.ModItems;
 import net.gleb.testmod.world.entity.ModEntityType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Animal;
@@ -18,53 +11,37 @@ import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import org.jetbrains.annotations.Nullable;
 
-public class StandEntity extends TamableAnimal {
+
+public class StandEntity extends Animal {
     public StandEntity(EntityType<StandEntity> type, Level level) {
         super(type, level);
-        this.setTame(true);
     }
 
     public StandEntity(Level level, double x, double y, double z){
         this(ModEntityType.STAND_ENTITY.get(), level);
         setPos(x, y, z);
     }
+
     public StandEntity(Level level, BlockPos position){
         this(level, position.getX(), position.getY(), position.getZ());
-    }
-
-    @Nullable
-    @Override
-    public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob parent) {
-        return new StandEntity(level, this.blockPosition());
     }
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
-        this.goalSelector.addGoal(2, new BreedGoal(this,1.0D));
-        this.goalSelector.addGoal(3,new FollowParentGoal(this, 1.1D));
-        this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 6.0F));
-        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(7, new TemptGoal(this, 1.0D, Ingredient.of(ModItems.ZIRCON.get()),false));
-        this.goalSelector.addGoal(8, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, true));
+        this.goalSelector.addGoal(1, new TemptGoal(this, 1.0D, Ingredient.EMPTY, false));
     }
     public static AttributeSupplier.Builder createAttributer(){
         return Wolf.createAttributes();
     }
-    public static boolean canSpawn(EntityType<StandEntity> entityType, LevelAccessor levelAccessor,
-                                   MobSpawnType mobSpawnType, BlockPos blockPos, RandomSource randomSource){
-        return Animal.checkAnimalSpawnRules(entityType, levelAccessor, mobSpawnType, blockPos, randomSource);
 
-    }
+    @Nullable
     @Override
-    public void setTame(boolean tame){
-        super.setTame(tame);
+    public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
+        return null;
     }
-
-
+    public void StandAttack(Player player){
+    }
 }
